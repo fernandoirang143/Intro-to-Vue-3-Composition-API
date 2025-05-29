ac9ae3df45fc581119beed9c2c9f3b8ee6352403
fernandoirang143 <fernandoirang143@gmail.com>
2025-05-29 14:21:08 +0800

    my lesson 2 progress

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
-<script setup></script>
+<script setup>
+ import {ref} from 'Vue';
+ const product = ref('Socks');
+ const description = ref('These are great socks!')
+</script>
 
 <template>
   <div class="product-display">
     <div class="product-container">
       <div class="product-info">
-        <h1>Socks</h1>
+        <h1>{{ product }}</h1>
+        <p>{{ description }}</p>
       </div>
     </div>
   </div>
